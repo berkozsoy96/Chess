b15cc034b901cfe6058b063cf51d40631373409b
Berk Özsoy <berk.ozsoy@ibss.com.tr>
2023-02-02 09:54:12 +0300

    adds pawn logic

--- ui.py
+++ ui.py
@@ -32,24 +32,31 @@ class Tile(pygame.sprite.Sprite):
 
 
 class Piece(pygame.sprite.Sprite):
-    def __init__(self, piece_symbol, size, position):
+    def __init__(self, piece_symbol: str, size: int, position: tuple[int, int]):
         pygame.sprite.Sprite.__init__(self)
         self.piece_symbol = piece_symbol
-        self.size = size
         self.position = position
+        self.color = "w" if self.piece_symbol.isupper() else "b"
+        self.size = size
         self.on_mouse = False
+        
+        self.legal_moves = []
 
         self.image = piece_images[piece_symbol]
         self.rect = self.image.get_rect()
-        self.rect.x = position[0]*self.size
-        self.rect.y = position[1]*self.size
+        self.rect.y = position[0]*self.size
+        self.rect.x = position[1]*self.size
     
+    def calculate_legal_moves(self):
+        print("here")
+        pass
+
     def update(self, mouse_pos, event_type):
         if event_type == pygame.MOUSEBUTTONDOWN:
             if self.rect.left <= mouse_pos[0] <= self.rect.right and \
                 self.rect.top <= mouse_pos[1] <= self.rect.bottom:
                 self.on_mouse = True
-                # show possible moves
+                print(self.legal_moves)
         
         if self.on_mouse:
             self.rect.x = mouse_pos[0] - self.size//2
@@ -58,14 +65,53 @@ class Piece(pygame.sprite.Sprite):
         if event_type == pygame.MOUSEBUTTONUP:
             if self.rect.left <= mouse_pos[0] <= self.rect.right and \
                 self.rect.top <= mouse_pos[1] <= self.rect.bottom:
-                # check possible moves
                 # check captures
-
-                position_x = mouse_pos[0]//self.size
                 position_y = mouse_pos[1]//self.size
-                self.rect.x = position_x*self.size
-                self.rect.y = position_y*self.size
+                position_x = mouse_pos[0]//self.size
+                if (position_y, position_x) in self.legal_moves:
+                    self.position = (position_y, position_x)
+                    self.rect.y = position_y*self.size
+                    self.rect.x = position_x*self.size
+                    self.calculate_legal_moves()
+                elif (position_y, position_x) == self.position:
+                    self.rect.x = self.position[1]*self.size
+                    self.rect.y = self.position[0]*self.size
+                    print("same place")
+                else:
+                    self.rect.x = self.position[1]*self.size
+                    self.rect.y = self.position[0]*self.size
+                    print("illegal")
                 self.on_mouse = False
+
+
+class Pawn(Piece):
+    def __init__(self, piece_symbol, size, position):
+        super().__init__(piece_symbol, size, position)
+        self.calculate_legal_moves()
+    
+    def calculate_legal_moves(self):
+        print("pawn")
+        # TODO: need to know all the pieces on the 
+        # board to calculate possible moves correctly
+        match (self.color, self.position[0]):
+            case ("w", 6):
+                self.legal_moves = [
+                    (5, self.position[1]),
+                    (4, self.position[1])
+                ]
+            case ("w", rank):
+                self.legal_moves = [
+                    (rank-1, self.position[1])
+                ]
+            case ("b", 1):
+                self.legal_moves = [
+                    (2, self.position[1]),
+                    (3, self.position[1])
+                ]
+            case ("b", rank):
+                self.legal_moves = [
+                    (rank+1, self.position[1])
+                ]
 
 
 class Game:
@@ -93,9 +139,14 @@ class Game:
             for j in range(8):
                 color = light_color if (i + j) % 2 == 0 else dark_color
                 self.tiles.add(Tile(color, size, (i, j)))
-                piece_char = default_board[j][i]
-                if piece_char != " ":
-                    self.pieces.add(Piece(piece_char, size, (i, j)))
+                match default_board[i][j]:
+                    case " ":
+                        continue
+                    case "p" | "P":
+                        new_piece = Pawn(default_board[i][j], size, (i, j))
+                    case char:
+                        new_piece = Piece(char, size, (i, j))
+                self.pieces.add(new_piece)
 
     def main_loop(self):
         running = True
