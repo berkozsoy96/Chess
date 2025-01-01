01f8ab0a921fb6772710635553b0eaa187e843d0
Berk Özsoy <berkozsoy34@gmail.com>
2025-01-02 00:43:06 +0300

    test

--- test.py
+++ test.py
@@ -9,14 +9,15 @@ def move_gen_test(depth, chess: Chess):
     positions = 0
     for move in moves:
         chess.make_move(move)
-        positions += move_gen_test(depth-1, chess)
+        result = move_gen_test(depth - 1, chess)
+        if depth == 5:
+            print(move, result)
+        positions += result
         chess.undo_move()
     return positions
 
-    
 
-for d in range(1, 6):
-    chess = Chess()
-    start = time.time()
-    result = move_gen_test(d, chess)
-    print(f"Depth: {d}, Positions: {result}, Time: {time.time() - start}")
+chess = Chess(fen="8/8/7p/3KNN1k/2p4p/8/3P2p1/8 w - - 0 1")
+start = time.time()
+result = move_gen_test(5, chess)
+print(f"Depth: {5}, Positions: {result}, Time: {time.time() - start}")
